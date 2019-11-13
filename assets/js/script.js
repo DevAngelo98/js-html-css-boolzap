b4b22a5b827e45188957494364ad28ee23766591
DevAngelo98 <ainglima07@gmail.com>
2019-11-13 15:53:32 +0100

    risposta dopo 1 secondo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
 
+  //Function bot
+  function bot(){
+    var elementMsgBot = $("#template-message .user-message-text").clone().text("ok");
+    $("#container_messages").append(elementMsgBot);
+  }
+
   $("#sendmessage").click(function(){  
     $("#micblock").addClass("mic");
     $(".send").attr('id',"sendbutton");
   });
 
   $(".send").click(function(){
-
     var textMessage = $("#sendmessage").val();
-    textMessage.replace(/\s+/g," ");
-    while(textMessage.lenght>20){
-       
-    }
-    
     var elementMsg = $("#template-message .my-message-text").clone();
-    var elemeText = elementMsg.html(textMessage);
-   
-    $("#container_messages").append(elemeText);
+    var elementText = elementMsg.text(textMessage);
+    $("#container_messages").append(elementText);
+    $("#sendmessage").val("");
+    setTimeout(bot, 1000);
   });
 
 
