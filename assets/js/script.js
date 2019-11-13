2bb0c8665a9b92551566039b26b45d864e916f6f
DevAngelo98 <ainglima07@gmail.com>
2019-11-13 18:05:46 +0100

    user search

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,30 @@ $(document).ready(function(){
   });
 
 
+  $("#search").keyup(function(){
+   var inputSearch = $(this).val().toUpperCase();
+   console.log(inputSearch);
+   
+   searchUser(inputSearch);
+  });
 
+  // Search user
+  function searchUser (input){
+
+    if(input.lenght!=0){
+      $(".user-container").each(function(){
+        // console.log($(this).find("h5").text());
+      if(!$(this).find("h5").text().toUpperCase().includes(input)){
+        $(this).addClass("none");
+        // $(".user-container").not($(this)).addClass("ok");
+      } else {
+        $(this).removeClass("none");
+      }
+      });
+    }
+    
+
+  }
+  
 
 });
