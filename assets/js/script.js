ddd59a6d07301a4c2a09b8d0faa8cd3a87f373a4
DevAngelo98 <ainglima07@gmail.com>
2019-11-14 20:44:01 +0100

    chat change (bug bot)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-
+  // var arrayMsg = new Array();
   //Function bot
   function bot(){
     var elementMsgBot = $("#template-message .user-message-text").clone().text("ok");
     $("#container_messages").append(elementMsgBot);
+    // arrayMsg = [];
+    // arrayMsg.push($("#container_messages *").clone());
+    for(var i=0; i<containerUser.length; i++){
+      var nameTemp=$("#icontemp").find("h5").text();
+      var nameArray = containerUser[i].name;
+      if(nameTemp==nameArray){
+        containerUser[i].msg.push($("#container_messages *").clone());
+      }
+    }
+    
   }
 
   // Change icon to click
@@ -42,20 +52,41 @@ $(document).ready(function(){
     });
   }
   
+  //Object
+  var userObj = {
+    name: "",
+    msg: []
+  };
 
-  
+  var containerUser = new Array();
+
   $(".user-container").each(function(){
+    var containerEmpty = $("#container_messages").clone();
+    var nameUser = $(this).find("h5").text();
+    var user = Object.create(userObj);
+    user.name = nameUser;
+    user.msg.lenght=0;
+    containerUser.push(user);
+
     $(this).click(function(){
       $(".user-container").not($(this)).removeClass("backgroundtemp");
       $(this).addClass("backgroundtemp");
       var nameTemp = $(this).find("h5").text();
       var imgTemp = $(this).find("img").clone();
       $("#icontemp #namereplace").text(nameTemp);    
-      $("#icontemp img").replaceWith(imgTemp);  
+      $("#icontemp img").replaceWith(imgTemp);
+      for(var i=0; i<containerUser.length; i++){
+        var nameArray = containerUser[i].name;
+        if(nameTemp==nameArray && containerUser[i].msg.lenght>0){
+          // console.log(containerUser[i].msg.lenght);
+          // $("#container_messages").replaceWith(containerUser[i].msg);        
+        } else if (containerUser[i].msg.lenght==0){
+          $("#container_messages").replaceWith(containerEmpty);
+          console.log("SI");
+        }
+      }
+      
     });
   });
-
-  
-  
 
 });
